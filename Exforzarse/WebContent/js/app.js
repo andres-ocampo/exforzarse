186817815679340d46137458146f0cbea0d8f378
Andres <Andres@PipePC.lan>
2017-10-16 08:43:25 -0500

    any address

---
+++ Exforzarse/WebContent/js/app.js
@@ -0,0 +1,39 @@
+var app = angular.module("app",["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../paginas/parciales/pecho.html'
+        })
+        .when('/pecho', {
+            templateUrl: '../paginas/parciales/pecho.html'
+        })
+        .when('/hombros', {
+            templateUrl: '../paginas/parciales/hombros.html'
+        })
+        .when('/espalda', {
+            templateUrl: '../paginas/parciales/espalda.html'
+        })
+        .when('/triceps', {
+            templateUrl: '../paginas/parciales/triceps.html'
+        })
+        .when('/piernas', {
+            templateUrl: '../paginas/parciales/piernas.html'
+        })
+        .when('/abdomen', {
+            templateUrl: '../paginas/parciales/abdomen.html'
+        })
+        .when('/biceps', {
+            templateUrl: '../paginas/parciales/biceps.html'
+        })
+        .when('/antebrazo', {
+            templateUrl: '../paginas/parciales/antebrazos.html'
+        });
+    
+});
+
+// create the controller and inject Angular's $scope
+app.controller('mainController', function ($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
